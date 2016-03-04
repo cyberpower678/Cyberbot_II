4340be0de2839a8e6f9d2cd3201e821044ef0747
System Administrator <niharikakohli29@gmail.com>
2016-03-04 20:41:20 +0530

    Add check and test for redirect to root

--- tests/checkIfDeadTest.php
+++ tests/checkIfDeadTest.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once dirname(__FILE__) . '/../IABot/checkIfDead.php';
+
 class checkIfDeadTest extends PHPUnit_Framework_TestCase {
 
 	public function testDeadLinkFalse() {
@@ -20,6 +22,12 @@ class checkIfDeadTest extends PHPUnit_Framework_TestCase {
 		$obj = new checkIfDead();
 		$res = $obj->checkDeadlink( 'https://en.wikipedia.org/w/index.php?title=Republic_of_India' );
 		$this->assertFalse( $res );
+	}
+
+	public function testRedirectToRoot() {
+		$obj = new checkIfDead();
+		$res = $obj->checkDeadlink( 'http://findarticles.com/p/articles/mi_m0FCL/is_4_30/ai_66760539/' );
+		$this->assertTrue( $res );
 	}
 
 }
