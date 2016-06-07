322b1931e926e119ef385b2c33daccc7aff1e0fb
MusikAnimal <ziemba0123@gmail.com>
2016-06-06 20:13:30 -0400

    Rework cleanUrl to remove URL fragment and all schemes, add tests

--- tests/checkIfDeadTest.php
+++ tests/checkIfDeadTest.php
@@ -23,11 +23,28 @@ class checkIfDeadTest extends PHPUnit_Framework_TestCase {
 					'https://en.wikipedia.org/wiki/Main_Page',
 					'https://en.wikipedia.org/w/index.php?title=Republic_of_India',
 					'https://astraldynamics.com',
-					'http://www.eonline.com/au/news/386489/2013-grammy-awards-winners-the-complete-list'
+					'http://www.eonline.com/au/news/386489/2013-grammy-awards-winners-the-complete-list',
+					'http://flysunairexpress.com/#about'
 				);
 		$result = $obj->checkDeadlinks( $urls );
-		$expected = array( false, false, false, false );
+		$expected = array( false, false, false, false, false );
 		$this->assertEquals( $result['results'], $expected );
+	}
+
+
+	public function testCleanUrl() {
+		$obj = new checkIfDead();
+
+		// workaround to make private function testable
+		$reflection = new \ReflectionClass( get_class( $obj ) );
+		$method = $reflection->getMethod('cleanUrl');
+		$method->setAccessible( true );
+
+		$this->assertEquals( $method->invokeArgs( $obj, array( 'http://google.com?q=blah' ) ), 'google.com?q=blah' );
+		$this->assertEquals( $method->invokeArgs( $obj, array( 'https://www.google.com/' ) ), 'google.com' );
+		$this->assertEquals( $method->invokeArgs( $obj, array( 'ftp://google.com/#param=1' ) ), 'google.com' );
+		$this->assertEquals( $method->invokeArgs( $obj, array( '//google.com' ) ), 'google.com' );
+		$this->assertEquals( $method->invokeArgs( $obj, array( 'www.google.www.com' ) ), 'google.www.com' );
 	}
 
 }
